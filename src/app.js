b72d84a969e9d0f4655ae0c558a483f262fc60a9
shahzaib <shahzaibshahg90@example.com>
2026-04-23 22:42:08 +0500

    another commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import paymentCheckoutRouter from "./routes/paymentCheckout.route.js";
 
 const app = express();
  
-app.use(cors({origin: "https://surplusfood-frontend.onrender.com", credentials: true,}));
+//app.use(cors({origin: "http://localhost:5173", credentials: true,}));
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true,}));
 
 app.use("/api/v1/webhook", paymentWebhookRouter);
 
